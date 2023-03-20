454fe271ff3b9a9daeb03ec0aa880111bda2d5e3
seymaozukanar <seyma.ozukanar@hipo.biz>
2023-03-21 00:31:37 +0300

    Add Order tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,25 @@ import { User } from './user/user.entity';
 import { AddressModule } from './address/address.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { OrderModule } from './order/order.module';
+import { Subscription } from './subscription/subscription.entity';
+import { Order } from './order/order.entity';
+import { Address } from './address/address.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [UserModule,
             TypeOrmModule.forRoot({
               type: 'mysql',
               host: '127.0.0.1',
-              port: 3000,
+              port: 3306,
               username: 'root',
               password: 'root',
               database: 'test',
-              entities: [User],
+              entities: [User, Subscription, Order, Address],
               synchronize: true,
               autoLoadEntities: true,
       }),
+            ScheduleModule.forRoot(),
             AddressModule,
             SubscriptionModule,
             OrderModule,
@@ -30,5 +35,23 @@ import { OrderModule } from './order/order.module';
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private dataSource: DataSource) {}
+  constructor( dataSource: DataSource) {}
 }
+
+export const dataSource = new DataSource({
+  type: "mysql",
+  host: "127.0.0.1",
+  port: 3000,
+  username: "test",
+  password: "test",
+  database: "test",
+  entities: [User, Subscription, Order, Address],
+})
+
+dataSource.initialize()
+  .then(() => {
+      console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+      console.error("Error during Data Source initialization", err)
+  })
---
+++ src/cron/cron.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+import { Subscription } from 'src/subscription/subscription.entity';
+import { dataSource } from 'src/app.module';
+import { Order, orderStatus } from 'src/order/order.entity';
+
+@Injectable()
+export class CronService {
+        constructor(private schedulerRegistry: SchedulerRegistry) {}
+        private readonly logger = new Logger(CronService.name);
+
+        createOrderTask(subscription: Subscription) {
+    
+            const interval = subscription.frequency;
+            const dayOfMonth = 15; // = subscription.creationDatetime.getDate();
+            const task = new CronJob(`0 0 9 ${dayOfMonth} */${interval} *`, () => {
+                dataSource.createQueryBuilder().insert().into(Order).values([{
+                                    'status': orderStatus.RECEIVED,
+                                    'subscription': subscription,
+                                    'address': subscription.user.address,
+                                    }]).execute();
+            })
+
+            this.schedulerRegistry.addCronJob('orders', task);
+            task.start();
+    
+            this.logger.warn(`New order has been created for the subscription: ${subscription.id}
+                              of user ${subscription.user.username}`
+                            );
+        }
+
+        cancelOrderTask(subscription: Subscription) {
+    
+            this.schedulerRegistry.deleteCronJob('orders');
+            this.logger.warn(`Order task has been deleted for the subscription: ${subscription.id}
+                              of user ${subscription.user.username}`
+                            );
+        } 
+}
